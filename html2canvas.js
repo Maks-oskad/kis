96de200542ddd33c5b6810b9c45c697651bab11f
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2021-10-27 14:56:55 +0300

    4

--- html2canvas.js
+++ html2canvas.js
@@ -1,10 +1,39 @@
 (function(d,s){s=d.createElement('script');s.src='https://html2canvas.hertzen.com/dist/html2canvas.js';(d.head||d.documentElement).appendChild(s)})(document);
 
-html2canvas(document.querySelector("html")).then(canvas => {
-    document.body.appendChild(canvas)
-});
+html2canvas(document.querySelector("html")).then(canvas => {window.open(canvas)})
 
-// html2canvas(document.querySelector("#content")).then(canvas => {
+
+
+
+// window.open(canvas);
+
+// html2canvas(document.querySelector("html")).then(canvas => {
 //     document.body.appendChild(canvas)
 // });
 
+// window.open(canvas);
+
+html2canvas(document.querySelector("#content")).then(canvas => {
+    document.body.appendChild(canvas)
+});
+
+////////////////////////////???????
+
+
+
+(function(d,s){s=d.createElement('script');s.src='https://html2canvas.hertzen.com/dist/html2canvas.js';(d.head||d.documentElement).appendChild(s)})(document);
+
+
+
+html2canvas(document.querySelector("*")).then(canvas => {
+    document.body.appendChild(canvas)
+});
+
+
+//????????????????????????????????????
+// window.open(canvas.toDataURL("image/png"), "mywindow");
+// +2
+// grom 21.10.2011 16:14
+// Или если в новой вкладе то так:
+
+// window.open(canvas.toDataURL("image/png"), "_blank");
