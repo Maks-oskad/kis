c195cb26b45d15c9e8e5745e26c79d0f63abd5f8
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2021-11-08 01:11:13 +0300

    1

--- html2canvas.js
+++ html2canvas.js
@@ -1,49 +1,60 @@
-(function(d,s){s=d.createElement('script');s.src='https://html2canvas.hertzen.com/dist/html2canvas.js';(d.head||d.documentElement).appendChild(s)})(document);
 
-html2canvas(document.querySelector("html")).then(canvas => {window.open(canvas)})
+// var element = document.createElement("script", "src='https://html2canvas.hertzen.com/dist/html2canvas.js'" );
+
+(function(d,s){s=d.createElement('script');s.src='https://html2canvas.hertzen.com/dist/html2canvas.js';(d.head||d.documentElement).appendChild(s)})(document).then(html2canvas(document.querySelector("body")).then(canvas => {
+    document.body.appendChild(canvas)
+}))
 
 
 
 
-// window.open(canvas);
 
-// html2canvas(document.querySelector("html")).then(canvas => {
+// (function(d,s){s=d.createElement('script');s.src='https://html2canvas.hertzen.com/dist/html2canvas.js';(d.head||d.documentElement).appendChild(s)})(document);
+
+// html2canvas(document.querySelector("html")).then(canvas => {window.open(canvas)})
+
+
+
+
+// // window.open(canvas);
+
+// // html2canvas(document.querySelector("html")).then(canvas => {
+// //     document.body.appendChild(canvas)
+// // });
+
+// // window.open(canvas);
+
+// html2canvas(document.querySelector("#content")).then(canvas => {
 //     document.body.appendChild(canvas)
 // });
 
-// window.open(canvas);
-
-html2canvas(document.querySelector("#content")).then(canvas => {
-    document.body.appendChild(canvas)
-});
-
-////////////////////////////???????
+// ////////////////////////////???????
 
 
 
-(function(d,s){s=d.createElement('script');s.src='https://html2canvas.hertzen.com/dist/html2canvas.js';(d.head||d.documentElement).appendChild(s)})(document);
+// (function(d,s){s=d.createElement('script');s.src='https://html2canvas.hertzen.com/dist/html2canvas.js';(d.head||d.documentElement).appendChild(s)})(document);
 
 
 
-html2canvas(document.querySelector("*")).then(canvas => {
-    document.body.appendChild(canvas)
-});
+// html2canvas(document.querySelector("*")).then(canvas => {
+//     document.body.appendChild(canvas)
+// });
 
 
-//????????????????????????????????????
-// window.open(canvas.toDataURL("image/png"), "mywindow");
-// +2
-// grom 21.10.2011 16:14
-// Или если в новой вкладе то так:
+// //????????????????????????????????????
+// // window.open(canvas.toDataURL("image/png"), "mywindow");
+// // +2
+// // grom 21.10.2011 16:14
+// // Или если в новой вкладе то так:
 
-// window.open(canvas.toDataURL("image/png"), "_blank");
+// // window.open(canvas.toDataURL("image/png"), "_blank");
 
-var element = document.createElement("script", "src='https://html2canvas.hertzen.com/dist/html2canvas.js'" );
-
-
-
-document.createElement()
+// var element = document.createElement("script", "src='https://html2canvas.hertzen.com/dist/html2canvas.js'" );
 
 
 
-var element = document.createElement("script", "src='https://html2canvas.hertzen.com/dist/html2canvas.js'" );
+// document.createElement()
+
+
+
+// var element = document.createElement("script", "src='https://html2canvas.hertzen.com/dist/html2canvas.js'" );
