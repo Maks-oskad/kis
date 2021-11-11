eb447da79343db4ce8842bf43f53056b3cf4f962
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2021-11-11 17:39:17 +0300

    1

--- attenDance.js
+++ attenDance.js
@@ -1,7 +1,7 @@
 
 
-document.querySelector("#container > div > main > div > div > section > div > div > div.styled__StyledCardContent-sc-1ydsk2u-4.kMuiUc > div > div > div > div.ant-tabs-content.ant-tabs-content-no-animated.ant-tabs-top-content > div.ant-tabs-tabpane.ant-tabs-tabpane-active > div.ant-spin-nested-loading > div > div > div > div > div.styled__StyledCardContent-sc-123jlgd-3.eEOoJL > div.ant-table-wrapper.sc-hEsumM.sc-bYwvMP.bBBBXy > div > div > div > div > div.ant-table-fixed-left > div > div > table > thead > tr > th > div").innerHTML+=
-`<button onclick = "attenDance()">Нажми меня</button>`
+document.querySelector("#container > div > main > div > header").innerHTML+=
+`<button style="color:white; background-color:#10b072" onclick = "attenDance()">Нажми меня</button>`
 
 
 
