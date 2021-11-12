3cac8dcc6f813d20d14a880e18dfa6a2e3791c87
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2021-11-12 09:35:32 +0300

    1

--- attenDanceButon.js
+++ attenDanceButon.js
@@ -1,5 +1,5 @@
 var htmlHeader = document.getElementsByTagName("head")[0];         
 var myScript = document.createElement('script');
 myScript.type = 'text/javascript';
-myScript.src = 'myscriptlocation.js';
+myScript.src = 'https://raw.githubusercontent.com/Maks-oskad/kis/main/attenDance.js';
 htmlHeader.appendChild(myScript);
