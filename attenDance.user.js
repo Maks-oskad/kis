0b4de392cf8e748fe7808dcc4d9b9c9b205dd225
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2021-11-14 14:03:19 +0300

    final2

--- attenDance.user.js
+++ attenDance.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://smd.mos.ru/groups/2032230/attendance
+// @match        https://smd.mos.ru/groups/*/attendance
 // @icon         https://www.google.com/s2/favicons?domain=mos.ru
 // @grant       unsafeWindow
 // ==/UserScript==
