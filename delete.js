dffb3a0a611a0c47ba9fd214cd03cc4f71d941a6
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2022-05-30 19:53:52 +0300

    d

---
+++ delete.js
@@ -0,0 +1,71 @@
+let BD
+
+function getF(getSting) {
+
+    xhr = new XMLHttpRequest();
+    xhr.open('GET',
+       getSting);
+ 
+    var el = window.location.href
+    var numEl = parseInt(el.match(/\d+/))
+ 
+    xhr.onload = function () {
+        BD = JSON.parse(xhr.responseText);
+        console.log(BD, 'это из запроса гет не функции')
+        // BD.participants.forEach((e) => {
+
+     };
+                
+               // xhr.send();
+          
+             
+
+
+
+        
+
+
+    }
+
+
+
+
+
+
+function statusRegistry(){
+for(let i =101; i < 1000; i++){
+    try {
+    getF(`https://smd.mos.ru/api/v1/groups/${i}/status_registry`)
+    if(BD.data.status.key !== "CLOSED")
+    {console.log("закрыта", i)}
+    else
+    {console.log(BD.data.status.key)}
+     } catch (err) {
+console.log('error')
+    // обработка ошибки
+  
+  }
+}
+
+}
+
+
+
+xhr.onload = function () {
+    BD = JSON.parse(xhr.responseText);
+    BD.participants.forEach((e) => {
+       i = 0
+       j = 0
+       if (e.id == numEl) {
+          i = 0
+          e.lessons.forEach((e) => {
+             console.log(e)
+          
+          })
+
+          if ((i > 0) || (j > 0)) { console.log("+" + i + " -" + j) }
+          else { console.log("0 0") }
+
+       }
+    });
+ };
