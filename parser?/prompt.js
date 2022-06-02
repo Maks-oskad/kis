2e5bf6e99dce4e318ae00ffccc3722b231f4ca96
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2022-06-02 20:21:33 +0300

    02/06/2022

--- parser?/prompt.js
+++ parser?/prompt.js
@@ -10,7 +10,16 @@ strZapros = prompt(`
 ------------------------------------------------------`,`G-00005657
 G-02043344`)
 //форматирование
+arrZapros = arrZapros.replace("G-","")
 arrZaprosFormated = arrZapros.split("\n")
+arrZaprosFormated.map((e)=>{parseInt(e,10)})
 //отчистка
-arrZapros.replace(/[+\n+\r+\,]+/g, '')
+//arrZapros.replace(/[+\n+\r+\,]+/g, '')
 //
+
+
+
+
+arrZapros.replace("G-00000","").replace("G-0000","")
+.replace("G-000","").replace("G-00","")
+.replace("G-0","").replace("G-","")
