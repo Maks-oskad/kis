b72a118cb53ce2cff67c0368e26cc0b65d8ab898
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2022-06-14 22:48:11 +0300

    хзщрж

--- parser?/PERESECHENIA/PERESECHENIA_FINAL.js
+++ parser?/PERESECHENIA/PERESECHENIA_FINAL.js
@@ -1,3 +1,4 @@
+{
 let arrZapros, strZapros, arrZaprosFormated
 //запрос ввода данных
 arrZapros = prompt(`
@@ -7,118 +8,12 @@ arrZapros = prompt(`
 ------------------------------------------------------
 (не обязательно указывать те группы которые пересекаются
 скрипт найдет эти группы сам)
-------------------------------------------------------`,`G-02043524
-G-00003101
-G-02046663
-G-02035347
-G-02036637
-G-00003101
-G-02037888
-G-02038427
-G-02043343
-G-02044109
-G-02035458
-G-02044066
-G-00002734
-G-02044111
-G-02048329
-G-02032419
-G-00000546
-G-02038438
-G-02043345
-G-02046663
-G-02035446
-G-02044108
-G-02043348
-G-02045066
-G-02046127
-G-02036744
-G-02044232
-G-02044093
-G-02043524
-G-02043346
-G-02046130
-G-02037480
-G-02043735
-G-02043351
-G-02044105
-G-02046639
-G-02042968
-G-02039270
-G-02043764
-G-02042552
-G-02035022
-G-02037238
-G-02036975
-G-02035471
-G-00005657
-G-00002737
-G-02038429
+------------------------------------------------------`,`G-02035022
 G-02042549
-G-02035247
-G-02025943
-G-02035749
-G-02032416
-G-02038437
-G-02029292
-G-02012658
-G-02025948
-G-02042554
-G-02035548
-G-02033716
-G-02025947
-G-02037180
-G-02034255
-G-02044091
-G-02038433
-G-02038430
-G-02036980
-G-02037118
-G-02035453
-G-02032417
-G-02037156
-G-02039274
-G-02043344
-G-02046672
+G-02044066
 G-02042934
-G-02043491
-G-02044090
-G-02046586
-G-02043338
-G-02052498
-G-02043350
-G-02043339
-G-02043347
-G-02043340
-G-02043349
-G-02043353
-G-02044107
-G-02042966
-G-02043737
-G-02043736
-G-02042964
-G-02044086
-G-02046129
-G-02048275
-G-02048423
-G-02048424
-G-02046654
-G-02048331
-G-02047374
-G-02048379
-G-02048420
-G-02048426
-G-02048428
-G-02048706
-G-02048710
-G-02049039
-G-02052092
-G-02048418
-G-02048419
-G-02044238
-G-02052500
-G-02052764
-G-02052732`)
+G-02035247
+G-02035471`)
 //форматирование
 // arrZapros.replace(/[\r+\,]+/g,'')
 arrZapros = arrZapros.replaceAll("G-","").replaceAll(/[+\r+\,]+/g,'')
@@ -136,31 +31,30 @@ let my_base = {}
 function pereborMassiva(idLesson_arr){
     for(let i = 0; i!=idLesson_arr.length ;i++){
         func1(idLesson_arr[i], i)
+        console.log('get')
         }}
-
-
-async function func1(idLesson_arr, i){
-    intId=parseInt(idLesson_arr,10)
-
-    let url = `https://smd.mos.ru/api/v1/groups/${intId}/attendance?from=01.06.2022&to=14.06.2022`;
-    let response = await fetch(url);
-
-    commits[i] = await response.json()
-    // commits[i] = await response.json() // читаем ответ в формате JSON
-    }
-
-pereborMassiva(idLesson_arr)
-
-
-let timerId = setInterval(() =>{ 
+        
+        async function func1(idLesson_arr, i){
+            intId=parseInt(idLesson_arr,10)
+            
+            let url = `https://smd.mos.ru/api/v1/groups/${intId}/attendance?from=01.06.2022&to=14.06.2022`;
+            let response = await fetch(url);
+            
+            commits[i] = await response.json()
+            // commits[i] = await response.json() // читаем ответ в формате JSON
+        }
+        
+        pereborMassiva(idLesson_arr)
+        
+        
+        let timerId = setInterval(() =>{ 
     if (commits.length == arrZaprosFormated.length){
         console.log(commits)
         console.log(arrZaprosFormated)
         clearInterval(timerId)
         func3()//AEEEEEEEEEEEEE
     }
-    }, 5000)
-
+}, 5000)
 
 function func3(){
     // Object.assign(my_base, arrZaprosFormated)
@@ -172,45 +66,28 @@ function func3(){
     
     })
 }
-
+}
 ////////////////////////////////////222222222222222222222222222222222
 ////////////////////////////////////222222222222222222222222222222222
 ////////////////////////////////////222222222222222222222222222222222
+{
 
 let SRAVNIVAEM = []
 let LESSONS = []
 // подсчет общего числа уроков
 let total_length = 0
 
-
 function func4 (){commits.forEach((e)=>{ 
     total_length += e.lessons.length
     e.lessons.forEach((e)=>{ 
-        
-        LESSONS.push(e)
-        // Object.defineProperty(LESSONS[1], 3, { value:  44, writable: true })
-        
+        LESSONS.push(e)   
     })
 })}
 
+func4()
 
 
-    func4()
-
-
-// function func5 (){commits.forEach((e)=>{
-
-//сравнение уроков по дате
-//куда их сука сохранять и как
 test_obj = []
-
-// for(let k =0; k<total_length;k++){
-//     test_obj[k]={}
-// }
-
-
-
-
 
 for(i=0; i<total_length;i++){
 // let e1 =LESSONS[i]
@@ -254,8 +131,13 @@ test_obj[i] =  {
     }
 
 }
+
 for (let index = 0; index < total_length; index++){console.log(test_obj[index])}
 
+test_obj.filter(Boolean).forEach((e,i)=>{test_obj.filter(Boolean).forEach((e2,i2)=>
+    {if((e.a.lesson==e2.b.lesson)&&(e.b.lesson==e2.a.lesson))
+    { test_obj[i]= undefined;console.log("-")}console.log(e.a, e2.b,e.b,e2.a)})})
+    test_obj = test_obj.filter(Boolean)
 
 let batadata = JSON.stringify(commits)
 
@@ -274,61 +156,89 @@ let batadata = JSON.stringify(commits)
         };
     }());
   
-
+}
 // 333333333333333333333333333333333
-
+{
 
 let arrFINAL = []
 
-function FINAL(){
-commits.forEach((e1,i1)=>{
-    e1.participants.forEach((e2,i2)=>{
-        e2.lessons.filter(Boolean).forEach((e3,i3)=>{
-if (e3.presenceMark == "PRESENCE"){
-                    let AB_e31
-            test_obj.forEach((e31, i31)=>{
-                let true1 = 0
-                if(e31.a.g.id == e3.id){true1 = 1 ;AB_e31 = e31.b.g.id}
-                if(e31.b.g.id == e3.id){true1 = 1 ;AB_e31 = e31.a.g.id}
-                if (true1>0){
-                     //нашли а или б? и присваемаем ей найденное
-                    // if(i31>0){AB_e31 = e31.a.g.id}else{AB_e31 = e31.b.g.id}
+test_obj.forEach((t,i)=>{
+    commits.forEach((c2,i2)=>{
+        c2.participants.forEach((p3,i3)=>{
+            p3.lessons.forEach((l4,i4)=>{
+                console.log("lessons")
+            })
+        })
 
-                    console.log(i31,"нашли" ,e3.id, "НЕ НАШЛИ",AB_e31,  e31.a.g.id, e31.b.g.id)
-///////////////////////////////////////////////////////
-                    commits.forEach((be1,bi1)=>{
-                        be1.participants.forEach((be2,bi2)=>{
-                            if(be2.id == e2.id){
-                            be2.lessons.filter(Boolean).forEach((be3,bi4)=>{
-                                if (be3.presenceMark == "PRESENCE"){
-                    console.log("presence mark be3 ", be3,be1, e2)
+    })
+    console.log(arrFINAL)})
+
+
+for (let i1 = 0; i1 < test_obj.length; i1++) {
+    for (let i2 = 0; i2 < commits.length; i2++) {
+        for (let i3 = 0; i3 < commits.participantslength; i3++) {
+            const element = array[i3];
+            index
+        }
+    }
+    
+}
+
+
+// function FINAL(){
+// commits.forEach((e1,i1)=>{
+//     e1.participants.forEach((e2,i2)=>{
+//         e2.lessons.filter(Boolean).forEach((e3,i3)=>{
+// if (e3.presenceMark == "PRESENCE"){
+//                     let AB_e31
+//             test_obj.forEach((e31, i31)=>{
+//                 let true1 = 0
+//                 if(e31.a.g.id == e3.id){true1 = 1 ;AB_e31 = e31.b.g.id}
+//                 if(e31.b.g.id == e3.id){true1 = 1 ;AB_e31 = e31.a.g.id}
+//                 if (true1>0){
+//                      //нашли а или б? и присваемаем ей найденное
+//                     // if(i31>0){AB_e31 = e31.a.g.id}else{AB_e31 = e31.b.g.id}
+
+//                     console.log(i31,"нашли" ,e3.id, "НЕ НАШЛИ",AB_e31,  e31.a.g.id, e31.b.g.id)
+// ///////////////////////////////////////////////////////
+//                     commits.forEach((be1,bi1)=>{
+//                         be1.participants.forEach((be2,bi2)=>{
+//                             if(be2.id == e2.id){
+//                             be2.lessons.filter(Boolean).forEach((be3,bi4)=>{
+//                                 if (be3.presenceMark == "PRESENCE"){
+//                     console.log("presence mark be3 ", be3,be1, e2)
                                             
-                            if ((AB_e31 == be3.id)){
-                                console.log(AB_e31)
-                                console.log(e2.fio,be2.fio," " ,e2.id,"  ",e31.a.g.id," G-",e31.a.g.idGroup,e31.a.g.lessonDate, ` 
-                                ` ," ",e31.b.g.id," G-",e31.b.g.idGroup," ", e31.b.g.lessonDate)
+//                             if ((AB_e31 == be3.id)){
+//                                 console.log(AB_e31)
+//                                 console.log(e2.fio,be2.fio," " ,e2.id,"  ",e31.a.g.id," G-",e31.a.g.idGroup,e31.a.g.lessonDate, ` 
+//                                 ` ," ",e31.b.g.id," G-",e31.b.g.idGroup," ", e31.b.g.lessonDate)
                                 
-                                arrFINAL.push(["/",e2.fio,be2.fio,e2.id,e31.a.g.id,
-                                    " G-",e31.a.g.idGroup,e31.a.g.lessonDate,e31.b.g.id,e31.b.g.idGroup,e31.b.g.lessonDate])
+//                                 arrFINAL.push(["/",e2.fio,be2.fio,e2.id,e31.a.g.id,
+//                                     " G-",e31.a.g.idGroup,e31.a.g.lessonDate,e31.b.g.id,e31.b.g.idGroup,e31.b.g.lessonDate])
                                 
-//                                 commits.forEach((e9,i9)=>{
-//                                     e9.participants.forEach((e90,i90)=>{
-//                                           e90.lessons.filter(Boolean).forEach((e91,i91)=>{
-//                                             //   if (e91=e31.a.g.id){console.log(e91.idGroup, e91.lessonDate, e91.timeFrom, e91.timeTo)}
-//                                             //   if (e91=e31.b.g.id){console.log(e91.idGroup, e91.lessonDate, e91.timeFrom, e91.timeTo)}
-// })})})
+// //                                 commits.forEach((e9,i9)=>{
+// //                                     e9.participants.forEach((e90,i90)=>{
+// //                                           e90.lessons.filter(Boolean).forEach((e91,i91)=>{
+// //                                             //   if (e91=e31.a.g.id){console.log(e91.idGroup, e91.lessonDate, e91.timeFrom, e91.timeTo)}
+// //                                             //   if (e91=e31.b.g.id){console.log(e91.idGroup, e91.lessonDate, e91.timeFrom, e91.timeTo)}
+// // })})})
                                 
-                            }}})}})})}{console.log("yas0")}})}})})})
+//                             }}})}})})}{console.log("yas0")}})}})})})
 
 
-                            saveData(arrFINAL, 
-                                `${new Date().toLocaleString().slice(0,17) +document.title}.txt`)
-  console.log(arrFINAL)                          }
+//                             saveData(arrFINAL, 
+//                                 `${new Date().toLocaleString().slice(0,17) +document.title}.txt`)
+//   console.log(arrFINAL)                          }
 
-                            FINAL()
+//                             FINAL()
 
 
 
+
+
+
+
+                            }
 
 
 
