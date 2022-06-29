815165513d538fca8824c5b8871894024a8dd155
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2022-06-29 17:54:23 +0300

    29/06/2022

--- kis/уникальный по последней группе/1.js
+++ kis/уникальный по последней группе/1.js
@@ -4,7 +4,7 @@ https://smd.mos.ru/api/v1/participants/groups/805596
 
 
 
-arrCandidate = ['805596']
+arrCandidate = ['721247']
 // commits = [[],[]]
 // commits[1] = []
 
@@ -32,18 +32,30 @@ async function func1(idLesson_arr, ij){
     
     groups_All = await response.json()
     groups_ACTIVE = await response2.json()
+    await groups_All
+
+    
+    
+      
     // commits[i] = await response.json() // читаем ответ в формате JSON
 }
 
-arrCandidate.forEach((e,i) => 
+function f7(){arrCandidate.forEach((e,i) => 
 func1(e,i)
     );
-// commits[0][0] массив со всеми группами [ ]
-
+  }
+  async function f8() {
+    var x = await groups_ACTIVE;
+    // commits[0][0] массив со всеми группами [ ]
+    
+    console.log("YES"); // 10
+  }
 
 var active_groups_i = 0
 var groups_ACTIVE_TRUE= []
 // commits[0][1] = []
+let x,y
+function func9(){
 for (let i = 0; i < groups_All.length; i++) {
     if (groups_ACTIVE.includes(groups_All[i].id)) {//можно убрать эту ячейку, потом фильтр на (boolean)
         groups_ACTIVE_TRUE[active_groups_i] = groups_All[i]
@@ -54,37 +66,43 @@ for (let i = 0; i < groups_All.length; i++) {
 //заменить в датах символы 12 и 45
 }
 
-var f_date_formated = (date=>{
-    date_tmp = date.split(".")
-  return  new Date(date_tmp[2],date_tmp[1]-1,date_tmp[0])
-})
+
 
 
 // let x,y = new Date('0')
-var ACTIVE_GROUP_FINAL = groups_ACTIVE_TRUE[0]
+let ACTIVE_GROUP_FINAL = groups_ACTIVE_TRUE[0]
 x=f_date_formated(ACTIVE_GROUP_FINAL.firstEnrollmentDate)
 for (let index = 0; index < groups_ACTIVE_TRUE.length; index++)
 {
 y=f_date_formated(groups_ACTIVE_TRUE[index].firstEnrollmentDate)
 // console.log(x,y)
+console.log(x)
+console.log(y)
     if(x>y)
    {x=y
+    console.log(x)
     // { ACTIVE_GROUP_FINAL.firstEnrollmentDate=groups_ACTIVE_TRUE[index].firstEnrollmentDate
     // console.log (ACTIVE_GROUP_FINAL,groups_ACTIVE_TRUE[index].firstEnrollmentDate,x,y )
 }
 if (index+1 == groups_ACTIVE_TRUE.length){console.log(x)}
 
 }
-
+}
 
 var needed_dateS_OBJ
+function func10(){
 groups_ACTIVE_TRUE.forEach((E,i)=>{
     // console.log((f_date_formated(E.firstEnrollmentDate)),"brc",x)
     if(+f_date_formated(E.firstEnrollmentDate) == +x){
     console.log("yes")
     needed_dateS_OBJ = E
+    // return needed_dateS_OBJ
 }
 
-})
+})}
 
+let f_date_formated = (date=>{
+    date_tmp = date.split(".")
+  return  new Date(date_tmp[2],date_tmp[1]-1,date_tmp[0])
+})
 
