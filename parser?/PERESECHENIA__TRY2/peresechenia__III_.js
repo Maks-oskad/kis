8430d0d847ddd6f77fd01ad7ac338bcc5668a956
Maks-oskad <92970133+Maks-oskad@users.noreply.github.com>
2022-07-14 11:58:31 +0300

    14/07/2022

--- parser?/PERESECHENIA__TRY2/peresechenia__III_.js
+++ parser?/PERESECHENIA__TRY2/peresechenia__III_.js
@@ -10,7 +10,20 @@ async function func1(idLesson_arr, i){
 }
 
 
-let DB = []
+G-02034255
+G-02042934
+G-02044066
+G-02042549
+G-02042552
+G-02035471
+G-02035247
+G-02035453
+
+
+
+
+///////////////1
+let DB = {}
 lessons_my_arr_prompt = prompt("кода групп")
 lessons_my_arr_prompt = lessons_my_arr_prompt.replaceAll("G-","").replaceAll(/[+\r+\,]+/g,'').split("\n")
 
@@ -21,9 +34,44 @@ async function BOOK12(e,i){
         let intId=parseInt(e)
         let url = `https://smd.mos.ru/api/v1/groups/${intId}/attendance?from=01.06.2022&to=30.06.2022`
         let response = await fetch(url);
-        DB[i] = await response.json()
-        
+        DB[`${intId}`] = await response.json()
+        DB[`${intId}`].howmuch = 0
+        DB[`${intId}`].ID = intId
     };
+
+/////////2
+    Object.keys(DB).map(function(objectKey, index) {
+   
+        console.log(objectKey);
+        DB[objectKey].lessons.forEach((e,i)=>{
+            console.log(objectKey);
+            console.log(e);
+            DB[e.id] = []
+            DB[e.id][0] = objectKey
+            DB[e.id][1] = e.lessonDate
+            DB[e.id][2] = 0
+        })
+       
+        DB[objectKey].participants.forEach((e,i)=>{
+            e.lessons.forEach(e=>{
+                if(e.presenceMark == "PRESENCE" ){
+                    DB[DB[e.id][0]].howmuch++ 
+                    DB[e.id][2]+=1
+                }
+            })
+        })
+
+        });
+
+        
+/////////////////
+
+
+
+});
+
+
+
 
 
 DB.forEach((e,i)=>{
@@ -40,8 +88,10 @@ DB.forEach((e,i)=>{
     })
 })
 
+
 DB_lessons = {}
 DB[4].lessons.forEach((e,i)=>{
     i++
     DB_lessons[`${i}`] = e
 })
+
